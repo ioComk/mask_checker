f6072c5bece6ea27519d35def066b69665155035
ioComk <rw110407@gmail.com>
2020-12-10 19:32:23 +0900

    Optunaでパラメータ探索

--- training/dnn_training.py
+++ training/dnn_training.py
@@ -1,16 +1,17 @@
 import os
-from datetime import date
 import torch as t
 from torch import nn
 from torch.utils.data import DataLoader
 import torchvision.transforms as transforms
 from torchvision.datasets import ImageFolder
-from earlystopping import EarlyStopping
-from net import Net
 import uuid
 import optuna
+import pandas
 
-# Constant value -----
+from earlystopping import EarlyStopping
+from net import Net
+
+# Constant value
 MAX_EPOCH = 1000
 PATIENCE  = 20
 
@@ -18,7 +19,7 @@ PATIENCE  = 20
 def train_step(epochs, data_loader, model, optimizer):
 
     running_loss = 0.0
-    check_interval = 30
+    check_interval = 10
 
     model.train()
 
@@ -74,7 +75,7 @@ def val_step(model, data_loader, early_stopping):
         print('correct: {:d}  total: {:d}'.format(correct, total))
         print('accuracy = {:f}'.format(correct / total))
 
-        early_stopping(1-(correct/total), model)
+        early_stopping(correct/total, model)
         print('------------------------------------------')
         # print('Validation loss: %f' %(running_loss/itr))
 
@@ -89,10 +90,16 @@ def objective(trial):
 
     trial.set_user_attr('uuid', uuid_)
 
+    out_units = 2
+
     # ハイパーパラメータ
-    batch_size = trial.suggest_int('batch_size', 128, 512)
-    dropout    = trial.suggest_float('dropout', 0.1, 0.5)
-    lr         = trial.suggest_float('lr', 1e-5, 1e-2)
+    batch_size   = trial.suggest_int('batch_size', 32, 512)
+    dropout      = trial.suggest_float('dropout', 0.1, 0.5)
+    lr           = trial.suggest_float('lr', 1e-5, 1e-2)
+    mid_c        = trial.suggest_int('mid_c', 8, 16, 2)
+    out_c        = trial.suggest_int('out_c', 18, 32, 2)
+    hidden_units = trial.suggest_int('hidden_units', 1024, 4096)
+    out_units    = trial.suggest_int('out_units', 128, 256)
 
     # devフォルダにある画像を取り込み
     train_datasets = ImageFolder(root='dataset/dev/train', transform=data_transform['train'])
@@ -102,10 +109,15 @@ def objective(trial):
     train_dataloader = DataLoader(train_datasets, batch_size=batch_size, shuffle=True, num_workers=4)
     val_dataloader   = DataLoader(val_datasets,   batch_size=batch_size, shuffle=False, num_workers=4)
 
-    model = Net().to(device)
+    # データサイズを取得
+    in_c, h, w = train_datasets[0][0].shape
+
+    model = Net(in_c, h, w, mid_c, out_c, hidden_units, out_units, dropout).to(device)
 
     optimizer = t.optim.SGD(model.parameters(), lr)
-    early_stopping = EarlyStopping(PATIENCE, verbose=True, out_dir=out_dir)
+    early_stopping = EarlyStopping(PATIENCE, verbose=True, out_dir=out_dir, key='max')
+
+    acc = 0
 
     # DNN training -------------------------------------------------------
     for epoch in range(MAX_EPOCH+1):
@@ -124,6 +136,8 @@ def objective(trial):
         if trial.should_prune():
             raise optuna.exceptions.TrialPruned()
     # --------------------------------------------------------------------
+
+    return acc
 
 if __name__ == "__main__":
 
