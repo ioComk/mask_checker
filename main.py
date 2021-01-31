15474c8b179d29a18cae2b5a87311184af4fa576
ioComk <rw110407@gmail.com>
2021-01-31 23:07:10 +0900

    Complete

--- main.py
+++ main.py
@@ -8,6 +8,19 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials 
 from training.net import Net
 from datetime import datetime
+import picamera
+import picamera.array
+import smbus
+import time
+import RPi.GPIO as GPIO
+from sensor import ADConverterClass
+
+def read_temp(mode):
+    buf = bus.read_i2c_block_data(addr, mode, 3)
+    tmp = (buf[1] << 8) + buf[0]
+    temperature = ((tmp*2.0) - 27315.0) / (100.0)
+        
+    return temperature
 
 def check_deplicated(id):
     row = 0
@@ -35,6 +48,15 @@ def write_ss(row, id, temp):
 
 def main():
 
+    dist_sensor = ADConverterClass(ref_volts=VOLTS, ch=CH)
+
+    # Servo setup
+    servo_pin = 18
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(servo_pin, GPIO.OUT)
+    servo = GPIO.PWM(servo_pin, 50)
+    servo.start(0)
+
     device = 'cpu'
     
     # DNNモデル読み込み
@@ -51,98 +73,152 @@ def main():
                 transforms.Grayscale(),
                 ])
 
-    cap = cv2.VideoCapture(DEVICE_ID)
-
     # 顔検出モデル読込み
     detector = cv2.dnn.readNetFromCaffe(PROTOTXT_PATH, WEIGHTS_PATH)
 
-    # 初期フレームの読込
-    _, img = cap.read()
-
     # ウィンドウの準備
-    cv2.namedWindow(WINDOW_NAME)
+    cv2.namedWindow(WINDOW_NAME, cv2.WINDOW_NORMAL)
+    cv2.setWindowProperty(WINDOW_NAME, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 
     is_register = False
+    check_qr = False
+    counter = 0
+    id = 0
+    sensor = [0,0]
+    hand = False
 
-    # 変換処理ループ
-    while cap.isOpened():
+    begin_time = 0
 
-        qr = decode(img)
+    # 検温検知するためのリスト
+    temp_old = [0,0,0,0,0,0,0]
 
-        temperature = 33.3
-        print(is_register)
+    with picamera.PiCamera() as camera:
+        with picamera.array.PiRGBArray(camera) as stream:
+            camera.resolution = (1312, 800)
+            camera.rotation = -90
 
-        cv2.rectangle(img, (10, 10), (170, 50), BLACK, -1)
-        cv2.putText(img, 'ID: ', (10, 40), cv2.FONT_HERSHEY_SIMPLEX, 1.0, WHITE, 2, cv2.LINE_AA)
+            # メインループ  -------------------------------------
+            while True:
+                # stream.arrayにRGBの順で映像データを格納
+                camera.capture(stream, 'bgr', use_video_port=True)
 
-        if qr:
-            id = qr[0].data.decode('utf-8')
-            cv2.putText(img, id, (60, 40), cv2.FONT_HERSHEY_SIMPLEX, 1.0, WHITE, 2, cv2.LINE_AA)
-            row = check_deplicated(id)
-            if row != 0 and is_register is True:
-                cv2.putText(img, 'Registering now...', (180, 40), cv2.FONT_HERSHEY_SIMPLEX, 1.0, RED, 2, cv2.LINE_AA)
-                write_ss(row, id, temperature)
-                is_register = False
-            if row == 0:
-                cv2.putText(img, 'Already registered.', (180, 40), cv2.FONT_HERSHEY_SIMPLEX, 1.0, RED, 2, cv2.LINE_AA)
+                dist = dist_sensor.get_dist()
 
-        # 300x300に画像をリサイズ、画素値を調整
-        (h, w) = img.shape[:2]
-        blob = cv2.dnn.blobFromImage(cv2.resize(img, (300, 300)), 1.0,
-                                     (300, 300), (104.0, 177.0, 123.0))
-        # 顔検出の実行
-        detector.setInput(blob)
-        detections = detector.forward()
+                # 温度センサ読み取り
+                temp = read_temp(0x7) + 4
 
-        margin_x = [0, 0]
-        margin_y = [0, 0]
+                sensor[1] = sensor[0]
 
-        for i in range(0, detections.shape[2]):
-            confidence = detections[0, 0, i, 2]
-    
-            if confidence > CONFIDENCE:
+                # 手をかざした瞬間に立ち上がるフラグ
+                sensor[0] = 1 if dist < 8 else 0
+
+                if sensor[0] == 1 and sensor[1] == 0:
+                    begin_time = time.time()
+                    hand = True
+
+                # print(dist)
+
+                if hand:
+                    if time.time() - begin_time <= 1:
+                        servo.start(0)
+                        servo.ChangeDutyCycle(7)
+                    elif time.time() - begin_time > 1:
+                        servo.ChangeDutyCycle(5)
+                        # servo.stop()
+                        hand = False
+                        begin_time = 0
                 
-                box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-                (startX, startY, endX, endY) = box.astype('int')
+                # QR読み取り
+                qr = decode(stream.array)
 
+                cv2.rectangle(stream.array, (10, 10), (450, 90), BLACK, -1)
+                cv2.putText(stream.array, 'ID: ', (10, 80), cv2.FONT_HERSHEY_SIMPLEX, 3.0, WHITE, 3, cv2.LINE_AA)
 
-                face = img[startY-margin_y[0]:endY+margin_y[1], startX-margin_x[0]:endX+margin_x[1]]
+                if qr:
+                    if check_qr == False:
+                        id = qr[0].data.decode('utf-8')
+                        check_qr = True
+                temp_diff = 0
 
-                predicted = 0
+                for i in range(len(temp_old)):
+                    temp_diff += abs(temp - temp_old[i])
 
-                if face.shape[0]*face.shape[1] != 0:
-                
-                    face  = Image.fromarray(np.uint8(face))
-                    face  = transform(face)
-                    face  = face.view(1, face.shape[0], face.shape[1], face.shape[2])
+                # QRコードがかざされたら
+                if check_qr:
+                    cv2.putText(stream.array, id, (130, 80), cv2.FONT_HERSHEY_SIMPLEX, 3.0, WHITE, 3, cv2.LINE_AA)
+                    txt = str(round(temp,1))+' [Celsius]'
+                    cv2.putText(stream.array, txt, (480, 80), cv2.FONT_HERSHEY_SIMPLEX, 2.0, RED, 2, cv2.LINE_AA)
+                    if temp >= 34 and temp_diff <= 20:
+                        row = check_deplicated(id)
+                        if row != 0:
+                            # cv2.putText(stream.array, 'Registering now...', (180, 40), cv2.FONT_HERSHEY_SIMPLEX, 1.0, RED, 2, cv2.LINE_AA)
+                            write_ss(row, id, temp)
+                            check_qr = False
+                        elif row == 0:
+                            # cv2.putText(stream.array, 'Already registered.', (180, 40), cv2.FONT_HERSHEY_SIMPLEX, 1.0, RED, 2, cv2.LINE_AA)
+                            check_qr = False
 
-                    output = model(face)
+                # 300x300に画像をリサイズ、画素値を調整
+                (h, w) = stream.array.shape[:2]
+                blob = cv2.dnn.blobFromImage(cv2.resize(stream.array, (300, 300)), 1.0,
+                                             (300, 300), (104.0, 177.0, 123.0))
+                # 顔検出の実行
+                detector.setInput(blob)
+                detections = detector.forward()
 
-                    _, predicted = t.max(output.data, 1)
+                margin_x = [0, 0]
+                margin_y = [0, 0]
 
-                    # print(f'{output}, {predicted}')
+                for i in range(0, detections.shape[2]):
+                    confidence = detections[0, 0, i, 2]
 
-                if predicted == 1:
-                    cv2.rectangle(img, (startX-margin_x[0], startY-margin_y[0]), (endX+margin_x[1], endY+margin_y[1]), RED, thickness=2)
-                else:
-                    cv2.rectangle(img, (startX-margin_x[0], startY-margin_y[0]), (endX+margin_x[1], endY+margin_y[1]), GREEN, thickness=2)
+                    if confidence > CONFIDENCE:
 
-        # フレーム表示
-        cv2.imshow(WINDOW_NAME, img)
+                        box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+                        (startX, startY, endX, endY) = box.astype('int')
 
-        # Escキーで終了
-        key = cv2.waitKey(INTERVAL)
-        if key == ESC_KEY:
-            break
-        elif key == Q_KEY:
-            is_register = True
+                        face = stream.array[startY-margin_y[0]:endY+margin_y[1], startX-margin_x[0]:endX+margin_x[1]]
 
-        # 次のフレーム読み込み
-        _, img = cap.read()
+                        predicted = 0
+
+                        if face.shape[0]*face.shape[1] != 0:
+                        
+                            face  = Image.fromarray(np.uint8(face))
+                            face  = transform(face)
+                            face  = face.view(1, face.shape[0], face.shape[1], face.shape[2])
+
+                            output = model(face)
+
+                            _, predicted = t.max(output.data, 1)
+
+                        if predicted == 1:
+                            cv2.rectangle(stream.array, (startX-margin_x[0], startY-margin_y[0]), (endX+margin_x[1], endY+margin_y[1]), RED,         thickness=2)
+                        else:
+                            cv2.rectangle(stream.array, (startX-margin_x[0], startY-margin_y[0]), (endX+margin_x[1], endY+margin_y[1]), GREEN,       thickness=2)
+
+                temp_old[counter] = temp
+
+                counter += 1
+                if counter >= len(temp_old):
+                    counter = 0
+
+                # フレーム表示
+                cv2.imshow(WINDOW_NAME, stream.array)
+
+                # Escキーで終了
+                key = cv2.waitKey(INTERVAL)
+                if key == ESC_KEY:
+                    break
+
+                # streamをリセット
+                stream.seek(0)
+                stream.truncate()
 
     # 終了処理
     cv2.destroyAllWindows()
-    cap.release()
+    servo.stop()
+    dist_sensor.Cleanup()
+    GPIO.cleanup()
 
 if __name__ == "__main__":
 
@@ -192,6 +268,12 @@ if __name__ == "__main__":
     HIDDEN_UNITS = 3011
     OUT_UNITS = 128
     # -------------------------------------------------------------------------------------------------
+    CH = 0
+    VOLTS = 5
+
+    # 温度センサ
+    addr = 0x5a
+    bus = smbus.SMBus(1)
 
     today = datetime.now()
     today = today.strftime('%Y/%m/%d')
